48e78b636c633d1617f1c5025785aa2b277b4e8b
Girija Laxmi Iyer <30467482+girizaiyer@users.noreply.github.com>
2017-08-05 13:55:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var postOne = {
+    title : 'About Me || GIRIZA',
+    heading : 'Post One',
+    date : 'Aug 5th,2016',
+    content : 
+    `    <p>
+        After 4yrs of struggle with coding, i just realised this is the easiest thing ever!
+     </p>
+     <p>
+        Thanks to IMAD! :D
+     </p>`
+    
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+        <html>
+            <head>
+                <title>
+                ${title}
+                </title>
+            
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="container">
+                        <div>
+                             <a href="/">Home</a>
+                        </div>
+                        <hr/>
+                        <h1>
+                            ${heading}
+                        </h1>
+                        <div>
+                            ${date}
+                        </div>
+                        <div>
+                           ${content}
+                        </div>
+                </div>
+            </body>
+        </html>
+            
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/post1', function(req,res) {
-        res.sendFile(path.join(__dirname, 'ui', 'post1.html'));
+        res.send(createTemplate(postOne));
 });
 
 app.get('/post2',function(req,res){
