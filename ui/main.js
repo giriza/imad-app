b3774aca70ec0a35a3eac343e21bbb22a0619132
Girija Laxmi Iyer <30467482+girizaiyer@users.noreply.github.com>
2017-08-07 12:18:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change the text of main-text div
 var element = document.getElementById('main text');
 element.innerHTML = 'GIRIZA';
+
+//image movement
+var img = document.getElementById('img');
+img.onclick = function (){
+    
+    img.style.marginLeft ='100px';
+}
