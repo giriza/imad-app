b72377adfb2f47e534573f22d734dd01c558c6a2
Girija Laxmi Iyer <30467482+girizaiyer@users.noreply.github.com>
2017-08-07 12:30:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,14 @@ element.innerHTML = 'GIRIZA';
 
 //image movement
 var madi = document.getElementById('madi');
-madi.onclick = function (){
+var marginLeft = 0;
+function moveRight () {
+   marginLeft = marginLeft + 10;
+   img.style.marginLeft= marginLeft + 'px';
     
-    madi.style.marginLeft ='100px';
+    
 }
+madi.onclick = function (){
+    var interval =setInterval(moveRight , 100);
+
+};
