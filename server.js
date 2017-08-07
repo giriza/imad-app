96c06fb001753f0ca5a5027a1c175f6eff7123e8
Girija Laxmi Iyer <30467482+girizaiyer@users.noreply.github.com>
2017-08-07 16:25:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,12 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/:postName', function(req,res) {
     var postName = req.params.postName;
     res.send(createTemplate(articles[postName]));
