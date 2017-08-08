82c352045be5743ac1f67c77cbfbeb967007f871
Girija Laxmi Iyer <30467482+girizaiyer@users.noreply.github.com>
2017-08-08 22:06:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,15 +9,18 @@ var counter = 0;
   request.onreadystatechange = function () {
       if(request.readystate == XMLHttpRequest.DONE)
       {
-      //Take some action }
+          if(request.status==200)
+          {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
       //not yet done - no action reqd
   };
   //capture the response and store it in a variable
   
   //render the variable in correct span
-  counter = counter + 1;
   
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
     
 };
