b37af0dcc560094653bc9cb60e14dc4f3b5b6083
Girija Laxmi Iyer <30467482+girizaiyer@users.noreply.github.com>
2017-08-08 21:42:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ var counter = 0;
   //render the variable in correct span
   counter = counter + 1;
   
-  var span = document.getElementById('counter');
+  var span = document.getElementById('count');
   span.innerHTML = counter.toString();
     
 };
